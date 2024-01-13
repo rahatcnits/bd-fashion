1fdcb73b1728a53cf1dd6552b1f05311397f4a53
rahatcnits <rahatcnits@gmail.com>
2024-01-13 12:14:21 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Navbar
+const menuBox = document.querySelector(".menu-item");
+const navOpenBtn = document.querySelector(".nav-open");
+const closeBtn = document.querySelector(".close-btn");
+
+navOpenBtn.addEventListener("click", () => {
+  menuBox.style.left = "0px";
+  navOpenBtn.style.opacity = "0";
+});
+
+closeBtn.addEventListener("click", () => {
+  menuBox.style.left = "-100%";
+  navOpenBtn.style.opacity = "1";
+});
+
+// hero slider
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+});
