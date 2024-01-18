e5ab4ef4512f5ee6fb448b762e3079185754d0c6
Imtiaz Ahmed <ia64744@gmail.com>
2024-01-18 23:51:13 +0600

    added back to top button

--- js/app.js
+++ js/app.js
@@ -263,3 +263,23 @@ const qualityControlObserver = new IntersectionObserver((items) => {
 });
 
 qualityControlObserver.observe(qualityControl);
+
+//Get the button:
+mybutton = document.getElementById("bt-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    mybutton.style.display = "block";
+    mybutton.style.zIndex = 100;
+  } else {
+    mybutton.style.display = "none";
+  }
+}
